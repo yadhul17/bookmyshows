8278060af81d86bfe04892186c450ab6fb141ab8
yadhul17 <yadhul jayakumar@gmail.com>
2025-11-06 19:35:23 +0530

    updated

---
+++ bookmyshow/urls.py
@@ -0,0 +1,36 @@
+"""
+URL configuration for bookmyshow project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from app import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.index),
+    path('login/',views.loginuser),
+    path('register/',views.registeruser),
+    path('logout/',views.logoutuser),
+    path('viewall/<int:id>', views.vieww,name='viewall'),
+    path('accounts/', include('allauth.urls')),
+
+    
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
